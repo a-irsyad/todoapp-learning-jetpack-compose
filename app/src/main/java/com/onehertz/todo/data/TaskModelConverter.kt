3280f8f08400bfe71e9408c6153912b4861296f5
irsyad <airsyad@proton.me>
2024-12-01 18:05:41 +0800

    add: data model converter

--- app/src/main/java/com/onehertz/todo/data/TaskModelConverter.kt
+++ app/src/main/java/com/onehertz/todo/data/TaskModelConverter.kt
@@ -1,4 +1,51 @@
 package com.onehertz.todo.data
 
-class TaskModelConverter {
-}
+import com.onehertz.todo.data.source.local.LocalTask
+import com.onehertz.todo.data.source.network.NetworkTask
+import com.onehertz.todo.data.source.network.TaskStatus
+
+fun Task.toLocalTask() = LocalTask(
+    id = id,
+    title = title,
+    description = description,
+    isCompleted = isCompleted
+)
+
+fun List<Task>.toLocalTasks() = map(Task::toLocalTask)
+
+fun LocalTask.toTask() = Task(
+    id = id,
+    title = title,
+    description = description,
+    isCompleted = isCompleted
+)
+
+fun List<LocalTask>.toTasks() = map(LocalTask::toTask)
+
+fun NetworkTask.toTask() = Task(
+    id = id,
+    title = title,
+    description = shortDescription,
+    isCompleted = (status == TaskStatus.COMPLETE)
+)
+
+fun List<NetworkTask>.toTasks() = map(NetworkTask::toTask)
+
+fun Task.toNetworkTask() = NetworkTask(
+    id = id,
+    title = title,
+    shortDescription = description,
+    status = if (isCompleted) TaskStatus.COMPLETE else TaskStatus.ACTIVE
+)
+
+fun List<Task>.toNetworkTask() = map(Task::toNetworkTask)
+
+fun NetworkTask.toLocalTask() = LocalTask(
+    id = id,
+    title = title,
+    description = shortDescription,
+    isCompleted = (status == TaskStatus.COMPLETE)
+)
+
+
+
